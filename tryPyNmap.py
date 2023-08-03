50777b35596b85dae4de1095e9fcb8d9587e638c
viz-prakash <vjprakash0@gmail.com>
2023-08-03 08:09:34 -0400

    nmap

--- tryPyNmap.py
+++ tryPyNmap.py
@@ -4,27 +4,35 @@ import utils
 
 class PyNmapWrapper:
 
-    def single_scan(ip_port, timeout = None):
-        print("into single for %s", ip_port)
+    def single_scan(ip_port, arguments = None, timeout = 180):
+        print("into single for %s, timeout = %s", ip_port, timeout)
+
         nm = nmap.PortScanner()
         ip, port = ip_port
-        if timeout:
-            nm.scan(ip, str(port), timeout = timeout)
+        strp = str(port)
+
+        if arguments:
+            nm.scan(ip, strp, arguments = arguments, timeout = timeout)
         else:
-            nm.scan(ip, port)
+            nm.scan(ip, strp, timeout = timeout)
+
         print(nm.command_line())
         print(nm[ip])
+
+
+        print(nm.get_nmap_last_output())
+
         for key, value in nm[ip]['tcp'].items(): #因为只有一个，也就会直接返回唯一的一个
             return (ip, key, value['name'], value['version'])
 
     # 多次调用single_scan
-    def scan(ip_port_list, timeout = None):
+    def scan(ip_port_list, arguments = None, timeout = 180):
         result_collect = []
 
         for ip_port in ip_port_list:
             if timeout:
                 try:
-                    result = PyNmapWrapper.single_scan(ip_port, timeout)
+                    result = PyNmapWrapper.single_scan(ip_port, arguments = arguments, timeout = timeout)
                     result_collect.append(result)
                 except Exception as e:
                     result_collect.append((ip_port[0], ip_port[1], "", ""))
@@ -38,7 +46,9 @@ class PyNmapWrapper:
 
 if __name__ == '__main__':
     ip_port_list = utils.getDannyIPandPorts()
-    results = PyNmapWrapper.scan(ip_port_list[0:5], timeout = 10)
+    #results = PyNmapWrapper.scan(ip_port_list[0:5], timeout = 10)
+    results = PyNmapWrapper.scan([("192.168.87.1", 8080)], arguments = "-sV --script vuln", timeout = 300)
     for item in results:
         print(item)
+
 
