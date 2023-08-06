dd81c8ed18d8c871d5fa326b83ff2b6876540d16
viz-prakash <vjprakash0@gmail.com>
2023-08-06 11:13:36 -0400

    async nmap

---
+++ AsyncNmap.py
@@ -0,0 +1,94 @@
+import PythonNmap as nmap
+import json
+import datetime
+import time
+
+import utils
+
+# 改了源代码，
+def callback_print_and_record(host, ports, scan_data, output_file):
+    print("--------------------")
+    print(host)
+    print(ports)
+    print(scan_data)
+
+    if output_file:
+            with open(output_file, "a") as file:
+                file.write("----------------------------------------------------\n")
+                file.write(host)
+                file.write(" ")
+                file.write(ports)
+                file.write("\n")
+                if scan_data:
+                    json_scan_data = json.dumps(scan_data)
+                    file.write(json_scan_data)
+                else:
+                    file.write("PortScannerError")
+                file.write("\n")
+
+class PyNmapWrapper:
+
+    def __init__(self):
+        self.result_collect = []
+        self.batch_size = 4
+ 
+    def scan(self, ip_port_list, arguments = "-sV", timeout = 180, output_file = None):
+        start_time = time.time()
+        print("开始时间：", datetime.datetime.now())
+
+        result_collect = []
+        print("Many targets: ", str(len(ip_port_list)))
+        async_scanner_pool = []
+
+        batch_ip_port_list_collect = utils.split_array(ip_port_list, self.batch_size)
+        for batch_index in range(0, len(batch_ip_port_list_collect)):
+            print("batch = ", str(batch_index))
+
+            batch_ip_port_list = batch_ip_port_list_collect[batch_index]
+            for i in range(0, len(batch_ip_port_list)):
+
+                ip, port = batch_ip_port_list[i]
+                print("No.", str(i), ip, str(port))
+                # 默认有arguments和timeout
+
+                # 每轮里面都新建一个异步扫描器
+                this_async_scanner = nmap.PortScannerAsync()
+                async_scanner_pool.append(this_async_scanner)
+
+                #运行扫描，这不会被阻塞
+                this_async_scanner.scan(hosts = ip, ports = str(port), arguments = arguments, callback = callback_print_and_record, timeout = timeout, output_file = output_file)
+
+
+                #写文件的事晚点再说
+
+            #阻塞直到这个batch全部完成
+            running_sanner_count = len(async_scanner_pool)
+            while running_sanner_count > 0:
+                for scanner in async_scanner_pool:
+                    if not scanner.still_scanning():
+                        running_sanner_count = running_sanner_count - 1
+                        async_scanner_pool.remove(scanner)
+                print("waiting for", str(running_sanner_count), "running scan in this batch")
+                time.sleep(2)
+
+        print("All finished")
+        print("结束时间：", datetime.datetime.now())
+        print("运行用时：", str(time.time()-start_time))
+        
+        for result in result_collect:
+            print(result)
+
+        return result_collect
+
+
+if __name__ == '__main__':
+
+    #要导入端口扫描的结果来运行，最后输出结果到文件
+    danny_ip_port_list = utils.getDannyIPandPorts()
+    PyNmapWrapperInst = PyNmapWrapper()
+    keep_record_file = "asyncResult.txt"
+    #results = PyNmapWrapperInst.scan([('172.19.219.32', 7676), ('172.19.219.14', 8009), ('172.19.219.11', 631), ('172.19.221.34', 443)], arguments = "-sV --version-all --script vuln", timeout = 300, keep_record_file = keep_record_file)
+    results = PyNmapWrapperInst.scan(utils.getDannyIPandPorts(), arguments = "-sV --version-all --script vuln", timeout = 300, output_file = keep_record_file)
+    
+
+
--- tryPyNmap.py
+++ tryPyNmap.py
@@ -1,10 +1,19 @@
 import nmap
+import json
 
 import utils
 
+def callback_print_and_record(host, scan_result):
+    print("--------------------")
+    print(host)
+    print(scan_result)
+
 class PyNmapWrapper:
 
-    def single_scan(ip_port, arguments = None, timeout = 180):
+    def __init__(self):
+        self.result_collect = []
+
+    def single_scan(self, ip_port, arguments = None, timeout = 180, keep_record_file = None):
         print("into single for %s, timeout = %s", ip_port, timeout)
 
         nm = nmap.PortScanner()
@@ -18,37 +27,67 @@ class PyNmapWrapper:
 
         print(nm.command_line())
         print(nm[ip])
-
-
-        print(nm.get_nmap_last_output())
+        print(nm.get_nmap_last_output().decode())
 
         for key, value in nm[ip]['tcp'].items(): #因为只有一个，也就会直接返回唯一的一个
-            return (ip, key, value['name'], value['version'])
+            
+            if keep_record_file:
+                with open(keep_record_file, "a") as file:
+                    file.write("\n###########################################################################\n")
+                    file.write(ip)
+                    file.write(str(port))
+                    json_nmip = json.dumps(nm[ip])
+                    file.write(json_nmip)
+                    file.write("\n")
+                    file.write(nm.get_nmap_last_output().decode())
 
-    # 多次调用single_scan
-    def scan(ip_port_list, arguments = None, timeout = 180):
+            return (ip, key, value['name'], value['version'], nm.get_nmap_last_output().decode())
+
+ 
+    def scan(self, ip_port_list, arguments = "-sV", timeout = 180, keep_record_file = None):
+
         result_collect = []
+        print("Many targets: ", str(len(ip_port_list)))
+        async_scanner_pool = []
 
-        for ip_port in ip_port_list:
-            if timeout:
-                try:
-                    result = PyNmapWrapper.single_scan(ip_port, arguments = arguments, timeout = timeout)
-                    result_collect.append(result)
-                except Exception as e:
-                    result_collect.append((ip_port[0], ip_port[1], "", ""))
-                    print(e)
-            else:
-                result = PyNmapWrapper.single_scan(ip_port)
-                result_collect.append(result)
+        #先不分批
+        for i in range(0, len(ip_port_list)):
+
+            ip, port = ip_port_list[i]
+            print("No.", str(i))
+            # 默认有arguments和timeout
+
+            # 每轮里面都新建一个异步扫描器
+            this_async_scanner = nmap.PortScannerAsync()
+            async_scanner_pool.append(this_async_scanner)
+
+            #运行扫描，这不会被阻塞
+            this_async_scanner.scan(hosts = ip, ports = str(port), arguments = arguments, sudo = True, callback = callback_print_and_record, timeout = timeout)
+
+
+            #写文件的事晚点再说
+
+        #阻塞直到全部完成
+        running_sanner_count = len(async_scanner_pool)
+        while running_sanner_count > 0:
+            for scanner in async_scanner_pool:
+                if not scanner.still_scanning():
+                    running_sanner_count = running_sanner_count - 1
+                    async_scanner_pool.remove(scanner)
+            time.sleep(2)
+
+        print("All finished")
 
         return result_collect
 
 
 if __name__ == '__main__':
-    ip_port_list = utils.getDannyIPandPorts()
-    #results = PyNmapWrapper.scan(ip_port_list[0:5], timeout = 10)
-    results = PyNmapWrapper.scan([("192.168.87.1", 8080)], arguments = "-sV --script vuln", timeout = 300)
-    for item in results:
-        print(item)
+
+    #要导入端口扫描的结果来运行，最后输出结果到文件
+    danny_ip_port_list = utils.getDannyIPandPorts()
+    PyNmapWrapperInst = PyNmapWrapper()
+    keep_record_file = "quickResult.txt"
+    results = PyNmapWrapperInst.scan(danny_ip_port_list, arguments = "-sV --version-all --script vuln", timeout = 300, keep_record_file = keep_record_file)
+    
 
 
